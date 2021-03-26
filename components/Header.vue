c0ac032751709c92d7366defbf536f10c1ffa58c
kingflamez <flamekeed@gmail.com>
2021-03-26 02:17:16 +0100

    users search

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar">
+    <nuxt-link to="/"><img class="navbar__logo" src="@/assets/svgs/logo/github.svg" svg-inline /></nuxt-link>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+  background-color: $dark;
+  padding: 12px 32px;
+  text-align: center;
+  &__logo {
+    path {
+      fill: white;
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <main class="container">
+      <SearchResult v-if="$route.query.search" />
+      <SearchForm v-else />
+      <Footer />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
